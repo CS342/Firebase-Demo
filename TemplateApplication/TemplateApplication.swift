bde7007b33a77766e30fafae5f8dcb7545c18728
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-02-01 10:31:28 -0500

    Initial commit

---
+++ TemplateApplication/TemplateApplication.swift
@@ -0,0 +1,30 @@
+//
+// This source file is part of the Stanford CardinalKit Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import SwiftUI
+import TemplateOnboardingFlow
+import TemplateSharedContext
+
+
+@main
+struct TemplateApplication: App {
+    @UIApplicationDelegateAdaptor(TemplateAppDelegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .sheet(isPresented: !$completedOnboardingFlow) {
+                    OnboardingFlow()
+                }
+                .cardinalKit(appDelegate)
+        }
+    }
+}
